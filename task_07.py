37e08a44a363dc8d6ad2a6e7f64a173e128b5b66
rudikrudik <rudik.login@gmail.com>
2022-11-28 20:02:17 +0300

    chahge algo

--- task_07.py
+++ task_07.py
@@ -1,18 +1,10 @@
 def combine_anagrams(target_list: list) -> list:
-    temp_list = []
-    temp_word = []
+    temp_dict = {}
 
     for i in target_list:
-        if len(temp_word) == 0:
-            temp_word.append(i)
+        if not temp_dict.get(''.join(sorted(i))):
+            temp_dict[''.join(sorted(i))] = [i]
         else:
-            for j in target_list:
-                if temp_word[-1] != j and sorted(temp_word[-1]) == sorted(j):
-                    temp_word.append(target_list.pop(target_list.index(j)))
+            temp_dict[''.join(sorted(i))].append(i)
 
-            else:
-                temp_list.append(temp_word)
-                temp_word = []
-                temp_word.append(i)
-
-    return temp_list
+    return list(temp_dict.values())
