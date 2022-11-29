779895505063ee4b8ad4914988c212651bba45be
rudikrudik <rudik.login@gmail.com>
2022-11-29 15:10:47 +0300

    fix errors

--- task_06.py
+++ task_06.py
@@ -1,16 +1,19 @@
-def rps_game_winner(game_player: list) -> str:
+def rps_game_winner(game_player: list) -> list:
 
-    if len(game_player) > 2:
-        raise Exception('WrongNumberOfPlayersError')
+    try:
+        if len(game_player) > 2:
+            raise ValueError('WrongNumberOfPlayersError')
+        for i in game_player:
+            if i[1] not in 'RPS':
+                raise ValueError('NoSuchStrategyError')
+    except ValueError as e:
+        return e
 
-    for i in game_player:
-        if i[1] not in 'RPS':
-            raise Exception('NoSuchStrategyError')
-
-    if game_player[0][1] == game_player[1][1]:
-        return game_player[0]
-
-    if game_player[0][1] + game_player[1][1] in ('PR', 'SP', 'RS'):
-        return game_player[0]
     else:
-        return game_player[1]
+        if game_player[0][1] == game_player[1][1]:
+            return game_player[0]
+
+        if game_player[0][1] + game_player[1][1] in ('PR', 'SP', 'RS'):
+            return game_player[0]
+        else:
+            return game_player[1]
